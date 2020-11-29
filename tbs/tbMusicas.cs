1a4d9056ab8862e7d49a84a2d9a10c7c98867bd7
EsposaDoArnaldo <daysechioquetta@gmail.com>
2020-11-28 23:27:11 -0300

    Gravação da Hora da importação

--- tbs/tbMusicas.cs
+++ tbs/tbMusicas.cs
@@ -14,7 +14,7 @@ namespace XeviousPlayer2
         public long Tamanho { get; set; }
         public int Tempo { get; set; }      // Quantidade de segundos
         public int MaxVol { get; set; }     // Não sei se tem como guardar os volumas acho qe sim
-        public int TocadoEm { get; set; }
+        public string TocadoEm { get; set; }
         public int Pular { get; set; }
         public int Pulado { get; set; }
         public int CutIni { get; set; }
@@ -116,11 +116,13 @@ namespace XeviousPlayer2
             {
                 using (var cmd = DalHelper.DbConnection().CreateCommand())
                 {
-                    cmd.CommandText = "INSERT INTO Musicas(Nome, Lugar, Banda, Tempo) values (@Nome, @Lugar, @Banda, @Tempo)";
+                    cmd.CommandText = "INSERT INTO Musicas(Nome, Lugar, Banda, Tempo, Tamanho, TocadoEm) values (@Nome, @Lugar, @Banda, @Tempo, @Tamanho, datetime('now'))";
                     cmd.Parameters.AddWithValue("@Nome", Nome);
                     cmd.Parameters.AddWithValue("@Lugar", Lugar);
                     cmd.Parameters.AddWithValue("@Banda", Banda);
                     cmd.Parameters.AddWithValue("@Tempo", Tempo);
+                    cmd.Parameters.AddWithValue("@Tamanho", Tamanho);
+                    // cmd.Parameters.AddWithValue("@TocadoEm", AgoraSQL());
                     cmd.ExecuteNonQuery();
                 }
             }
@@ -128,50 +130,14 @@ namespace XeviousPlayer2
             {
                 throw ex;
             }
-
-            /* tbM.Nome = data.Title;
-            tbM.Ano = data.Year == null ? 0 : int.Parse(data.Year);
-            tbM.Tempo = data.Duration.Seconds;
-            tbM.Ano = int.Parse(data.Year);
-            tbM.Banda = data.Artist;
-            tbM.SetaGenero(data.Genre);
-            tbM.TemImagem = data.Image == null ? 0 : 1;
-            tbM.Tamanho = Tam;
-            tbM.Lugar = lugar; */
-
-            /* CREATE TABLE "Musicas"(
-                "IDMusica"  INTEGER UNIQUE,
-                "Nome"  TEXT NOT NULL,
-                "Lugar" TEXT NOT NULL,
-                "Banda" INTEGER,
-                "Tamanho"   INTEGER,
-                "BitRate"   INTEGER,
-                "Tempo" TEXT,
-                "MaxVol"    INTEGER,
-                "Equalizacao"   INTEGER,
-                "TocadoEm"  TEXT,
-                "Unid"  INTEGER,
-                "Pular" INTEGER,
-                "Pulado"    INTEGER,
-                "CutIni"    INTEGER,
-                "CutFim"    INTEGER,
-                PRIMARY KEY("IDMusica" AUTOINCREMENT)
-            ); */
-
-            //            public string Lugar { get; set; }
-            //public long Tamanho { get; set; }
-            //public int Tempo { get; set; }      // Quantidade de segundos
-            //public int MaxVol { get; set; }     // Não sei se tem como guardar os volumas acho qe sim
-            //public int TocadoEm { get; set; }
-            //public int Pular { get; set; }
-            //public int Pulado { get; set; }
-            //public int CutIni { get; set; }
-            //public int CutFim { get; set; }
-            //public int TemImagem { get; set; }
-            //private int lcAno = 0;
-            //private int lcBanda = 0;
-            //private string lcNome = "";
         }
+
+        /* private string AgoraSQL()
+        {
+            DateTime dAgora = new DateTime();
+            string Agora = string.Format("{0:yy-MM-dd}", dAgora);
+            return Agora;
+        } */
 
     }
 }
