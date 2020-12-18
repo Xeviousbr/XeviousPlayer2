adf0968a764f7ea9f4a770e21480f1fb927e316b
EsposaDoArnaldo <daysechioquetta@gmail.com>
2020-12-17 23:31:44 -0300

    Coloquei o icone

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -772,6 +772,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.SystemColors.GrayText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
